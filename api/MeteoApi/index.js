8e10b397f134e56d6be3faab9c77707557da8710
Ricardo Cordova <ricardocordovac@gmail.com>
2025-09-05 15:25:08 +0200

    fix npm build error with legacy-peer-deps and updated vercel.json

--- api/MeteoApi/index.js
+++ api/MeteoApi/index.js
@@ -1,13 +1,13 @@
 const { exec } = require('child_process');
-   const path = require('path');
+     const path = require('path');
 
-   module.exports = (req, res) => {
-     const dotnetPath = path.join(__dirname, 'MeteoApi.dll');
-     exec(`dotnet ${dotnetPath}`, (error, stdout, stderr) => {
-       if (error) {
-         console.error(`Error executing .NET: ${error}`);
-         return res.status(500).json({ error: 'Internal server error' });
-       }
-       res.status(200).json({ message: stdout || 'Datos actualizados' });
-     });
-   };
+     module.exports = (req, res) => {
+       const dotnetPath = path.join(__dirname, 'MeteoApi.dll');
+       exec(`dotnet ${dotnetPath}`, (error, stdout, stderr) => {
+         if (error) {
+           console.error(`Error executing .NET: ${error}`);
+           return res.status(500).json({ error: 'Internal server error' });
+         }
+         res.status(200).json({ message: stdout || 'Datos actualizados' });
+       });
+     };

a37940d9ae3e4a4a83402b914c32f115496d45d0
Ricardo Cordova <ricardocordovac@gmail.com>
2025-09-07 18:44:48 +0200

    Version Tabla sin chart.js funcionando

--- api/BACKDBSUPER/obj/Debug/net8.0/BACKDBSUPER.AssemblyInfo.cs
+++ api/BACKDBSUPER/obj/Debug/net8.0/BACKDBSUPER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26ab6b7f96387429b134d3d1d8a47f9a4cb34be4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0d7d962345dd82de550a6b7615c2c3680696ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WeatherChartComponent } from './weather-chart/weather-chart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WeatherChartComponent }
+];

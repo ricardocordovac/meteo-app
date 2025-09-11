767016c2e3c8ceda7bcf03bb54707869653155b5
Ricardo Cordova <ricardocordovac@gmail.com>
2025-09-11 02:14:22 +0200

    Version Ionic corregida v6

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,20 +1,17 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { WeatherChartComponent } from '../weather-chart/weather-chart.component'; // Ajusta la ruta si es necesario
-
 @Component({
   selector: 'app-tab3',
-   standalone: true,
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
-    imports: [
+  standalone: true,
+  imports: [
       IonHeader,
       IonToolbar,
       IonTitle,
       IonContent,
       WeatherChartComponent
     ],
+  templateUrl: './tab3.page.html',
+  styleUrls: ['./tab3.page.scss']
 })
-export class Tab3Page {
-  constructor() {}
-}
+export class Tab3Page {}
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app/app.component';
 import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
-import { routes } from './app/app.routes';
+import { routes } from './app/app.routes'; // Asegúrate de que este archivo exista
 
 bootstrapApplication(AppComponent, {
   providers: [

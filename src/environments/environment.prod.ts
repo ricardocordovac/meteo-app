ba844e9df9178c1dc8f3962f85b83e4a4dd9c5f4
Ricardo Cordova <ricardocordovac@gmail.com>
2025-09-12 00:15:21 +0200

    Version Ionic corregida v8

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+
   production: true,
   supabaseUrl: 'https://mdsirweaayrcqhyuxoxa.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kc2lyd2VhYXlyY3FoeXV4b3hhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMzE1MjIsImV4cCI6MjA3MjYwNzUyMn0.HsfMniSLxS7whm74kgajcA8fxwPIcR9-3kFTE1brDVU'

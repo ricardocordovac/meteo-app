f83db145fbe88fbbb64ca2f04083fcadd45d117f
Ricardo Cordova <ricardocordovac@gmail.com>
2025-09-13 07:43:48 +0200

    Version Ionic agregamos tab4

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit  } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent,IonSpinner,IonIcon } from '@ionic/angular/standalone';
+import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { CommonModule, DatePipe } from '@angular/common';
+import { SupabaseService } from '../services/supabase.service';
+import { sunny, cloud, rainy, snow } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+  standalone: true,
+    imports: [IonHeader,
+            IonToolbar,
+            IonTitle,
+            IonContent,
+            IonSpinner,
+            IonIcon,
+            ExploreContainerComponent,
+            CommonModule,
+            DatePipe],
+})
+export class Tab4Page implements OnInit  {
+  data: any[] = [];
+  error: string | null = null;
+  loading = true;
+
+  constructor(private supabaseService: SupabaseService) {
+    addIcons({ sunny, cloud, rainy, snow });
+  }
+
+  async ngOnInit() {
+    try {
+      this.data = await this.supabaseService.getDataByLocation('fuente_el_saz');
+    } catch (error: any) {
+      this.error = error.message || 'Error loading data';
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  getWeatherIcon(temp: number): string {
+    if (temp > 25) return 'sunny';
+    if (temp > 15) return 'cloud';
+    if (temp > 5) return 'rainy';
+    return 'snow';
+  }
+}

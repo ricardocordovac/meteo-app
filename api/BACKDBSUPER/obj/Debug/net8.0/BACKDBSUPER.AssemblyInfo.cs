9d9e56df7f86651762da6d0cc91665dc0fa164ad
Ricardo Cordova <ricardocordovac@gmail.com>
2025-09-20 18:54:37 +0200

    integracion template tinder v1

--- api/BACKDBSUPER/obj/Debug/net8.0/BACKDBSUPER.AssemblyInfo.cs
+++ api/BACKDBSUPER/obj/Debug/net8.0/BACKDBSUPER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4d68666b1c1a351d253ab08991100ce08a08f6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3d8ebcd87054fb4d2eedd9b3d43d9e036a8dcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

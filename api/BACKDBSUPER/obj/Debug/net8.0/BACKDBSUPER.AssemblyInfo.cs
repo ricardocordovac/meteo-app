2552be97b3dd6e13dfecf70a8356c6f04bb5dbbf
Ricardo Cordova <ricardocordovac@gmail.com>
2025-12-27 12:14:32 +0100

    Version 3.0 agregando mas iamgenes que faltaban

--- api/BACKDBSUPER/obj/Debug/net8.0/BACKDBSUPER.AssemblyInfo.cs
+++ api/BACKDBSUPER/obj/Debug/net8.0/BACKDBSUPER.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a645867b3f3c712a981462f9d82feeb6c03989cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd766b1f5ef0768dc753849e1d346871b457a32f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKDBSUPER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
